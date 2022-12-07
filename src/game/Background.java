f44926af37c9d6451a33b7b28852b26792f9a6c3
adrianismu <adrianismu263@gmail.com>
2022-12-07 16:24:58 +0700

    edit

--- src/game/Background.java
+++ src/game/Background.java
@@ -5,92 +5,87 @@ import java.awt.image.BufferedImage;
 import game.tools.ImageLoader;
 
 public class Background {
-	
+
 	private static final int LEFT_ROAD_BOUND = 140;
 	private static final int RIGHT_ROAD_BOUND = 582;
 	private static final int TOP_ROAD_BOUND = 0;
 	private static final int DOWN_ROAD_BOUND = 590;
-	
-	private	BufferedImage road, backRoad, backroad2, backroad3;
-	private int roadX, roadY, backRoadX, backRoadY; 
+
+	private BufferedImage road, backRoad, backroad2, backroad3;
+	private int roadX, roadY, backRoadX, backRoadY;
 	private int dy;
 
-	
-	public Background(){
-		roadX=160;
-	    roadY=1640;
-		 backRoadX = 100;
-		 backRoadY= -1000;
-	    
-	    road = ImageLoader.loadImage("/res/(long)portraitRoadBG.png");
-	    backRoad = ImageLoader.loadImage("/res/(long)background.png");
+	public Background() {
+		roadX = 160;
+		roadY = 1640;
+		backRoadX = 100;
+		backRoadY = -1000;
+
+		road = ImageLoader.loadImage("/res/(long)portraitRoadBG.png");
+		backRoad = ImageLoader.loadImage("/res/(long)background.png");
 		backroad2 = ImageLoader.loadImage("/res/(long)backgrounda.png");
-		backroad3 = ImageLoader.loadImage("/res/(long)backgrounda.png");
+		backroad3 = ImageLoader.loadImage("/res/(long)backgroundb.png");
 	}
 
-	 
-	 public int getRoadX(){
-		 return roadX;
-	 }
-	 
-	 public int getRoadY(){
-		 return roadY;
-	 }
-	 public int geBackRoadX(){
+	public int getRoadX() {
+		return roadX;
+	}
+
+	public int getRoadY() {
+		return roadY;
+	}
+
+	public int geBackRoadX() {
 		return backRoadX;
 	}
-	
-	public int getBackRoadY(){
+
+	public int getBackRoadY() {
 		return backRoadY;
 	}
-	 
-	 public Image getRoad() {	
+
+	public Image getRoad() {
 		return road;
 	}
-	 
-	 public Image getBackRoad(){
+
+	public Image getBackRoad() {
 		return backRoad;
-	 }
-
-	 public Image getBackRoad2(){
-		return backroad2;
-	 }
-
-	 public Image getBackRoad3(){
-		return backroad3;
-	 }
-	 
-	 
-	 public void update(){
-	     roadY+=dy;
-	     if(roadY>0){
-	    	 roadY=-3340;
-			 backRoadY+=dy;
-			 backRoadY+=dy;
-
-			
 	}
 
-	 }
-	 
-	 public void setSpeed(int dy){
-		 this.dy = dy;
-		 update();
-	 }
+	public Image getBackRoad2() {
+		return backroad2;
+	}
 
+	public Image getBackRoad3() {
+		return backroad3;
+	}
 
-	 public static int getLeftRoadBound() {
-		 return LEFT_ROAD_BOUND;
-	 }
+	public void update() {
+		roadY += dy;
+		if (roadY > 0) {
+			roadY = -3340;
+			backRoadY += dy;
+			backRoadY += dy;
 
-	 public static int getRightRoadBound() {
-		 return RIGHT_ROAD_BOUND;
-	 }
+		}
 
-	 public static int getTopRoadBound() {
+	}
+
+	public void setSpeed(int dy) {
+		this.dy = dy;
+		update();
+	}
+
+	public static int getLeftRoadBound() {
+		return LEFT_ROAD_BOUND;
+	}
+
+	public static int getRightRoadBound() {
+		return RIGHT_ROAD_BOUND;
+	}
+
+	public static int getTopRoadBound() {
 		return TOP_ROAD_BOUND;
 	}
-
 
 	public static int getDownRoadBound() {
 		return DOWN_ROAD_BOUND;
