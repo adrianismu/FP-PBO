1fdf9714373cc2915bdc982c52beff4ebaa4c32e
adrianismu <adrianismu263@gmail.com>
2022-12-18 21:14:26 +0700

    edit

--- src/game/Background.java
+++ src/game/Background.java
@@ -5,26 +5,25 @@ import java.awt.image.BufferedImage;
 import game.tools.ImageLoader;
 
 public class Background {
-	
+
 	private static final int LEFT_ROAD_BOUND = 150;
 	private static final int RIGHT_ROAD_BOUND = 570;
 	private static final int TOP_ROAD_BOUND = 0;
 	private static final int DOWN_ROAD_BOUND = 590;
-	
-	private	BufferedImage road, road2, backRoad, backroad2, backroad3 , end1;
-	private int roadX, roadY, backRoadX, backRoadY; 
+
+	private BufferedImage road, road2, backRoad, backroad2, backroad3, end1;
+	private int roadX, roadY, backRoadX, backRoadY;
 	private int dy;
 
-	
-	public Background(){
-		roadX=160;
-	    roadY=1640;
-		 backRoadX = 100;
-		 backRoadY= -1000;
-	    
-	    road = ImageLoader.loadImage("/res/(long)portraitRoadBG.png");
+	public Background() {
+		roadX = 160;
+		roadY = 1640;
+		backRoadX = 100;
+		backRoadY = -1000;
+
+		road = ImageLoader.loadImage("/res/(long)portraitRoadBG.png");
 		road2 = ImageLoader.loadImage("/res/(long)portraitRoadBGa.png");
-	    backRoad = ImageLoader.loadImage("/res/(long)background.png");
+		backRoad = ImageLoader.loadImage("/res/(long)background.png");
 		backroad2 = ImageLoader.loadImage("/res/(long)backgrounda.png");
 		backroad3 = ImageLoader.loadImage("/res/(long)backgroundb.png");
 		end1 = ImageLoader.loadImage("/res/end.jpg");
@@ -45,12 +44,12 @@ public class Background {
 	public int getBackRoadY() {
 		return backRoadY;
 	}
-	 
-	 public Image getRoad() {	
+
+	public Image getRoad() {
 		return road;
 	}
-		
-	public Image getRoadA() {	
+
+	public Image getRoadA() {
 		return road2;
 	}
 
--- src/game/states/StopState.java
+++ src/game/states/StopState.java
@@ -10,30 +10,30 @@ import game.GamePanel;
 import game.entities.Score;
 
 public class StopState extends GameState {
-	
+
 	private Score score;
-	
+
 	private Background background;
-	
+
 	Font highestScoreFont, f, titleFont;
-	
+
 	public StopState(GameStateManager gsm, Score score) {
-		
+
 		this.gsm = gsm;
-		
+
 		this.score = score;
-		
+
 		background = new Background();
-		
+
 		init();
 	}
 
 	@Override
 	public void init() {
-		highestScoreFont = new Font("HelveticaNeue",Font.BOLD,25);
-		f = new Font("HelveticaNeue",Font.BOLD,50);
-		titleFont = new Font("HelveticaNeue",Font.BOLD,60);
-		
+		highestScoreFont = new Font("HelveticaNeue", Font.BOLD, 25);
+		f = new Font("HelveticaNeue", Font.BOLD, 50);
+		titleFont = new Font("HelveticaNeue", Font.BOLD, 60);
+
 		score.setHighestScore(score.finalScore);
 	}
 
@@ -44,32 +44,31 @@ public class StopState extends GameState {
 
 	@Override
 	public void draw(Graphics g) {
-		
-		g.drawImage(background.getEnd(),0,0,null);
-		
+
+		g.drawImage(background.getEnd(), 0, 0, null);
+
 		g.setFont(highestScoreFont);
 		g.setColor(Color.WHITE);
-		g.drawString("Highest Score: "+score.getHighestScore(), 100, 400);
-		
+		g.drawString("Highest Score: " + score.getHighestScore(), 100, 400);
+
 		g.setFont(titleFont);
 		g.setColor(Color.WHITE);
-		g.drawString("GAME OVER",100 , 200);
-		
+		g.drawString("GAME OVER", 100, 200);
+
 		g.setFont(f);
 		g.setColor(Color.WHITE);
-		g.drawString("SCORE"+score.finalScore  ,100, 300);
-	
-		
-		g.setFont(new Font("HelveticaNeue",Font.PLAIN,25));
+		g.drawString("SCORE: " + score.finalScore, 100, 300);
+
+		g.setFont(new Font("HelveticaNeue", Font.PLAIN, 25));
 		g.setColor(Color.WHITE);
-		g.drawString("Press Space to Play Again", GamePanel.WIDTH/3 - 15, 480);
+		g.drawString("Press Space to Play Again", GamePanel.WIDTH / 3 - 15, 480);
 
 	}
 
 	@Override
 	public void keyPressed(int k) {
-		
-		if(k==KeyEvent.VK_SPACE) {
+
+		if (k == KeyEvent.VK_SPACE) {
 			gsm.setState(GameStateManager.LEVEL1STATE, score);
 		}
 	}
